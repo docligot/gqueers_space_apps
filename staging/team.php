908b0386cee5429a5e37f2830c2060bcfe3a87ae
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-05-31 05:41:30 +0800

    Temporary hide writeup until completed by team members

--- staging/team.php
+++ staging/team.php
@@ -8,17 +8,17 @@ function showTeam() {
 		<h1 class="w3-xlarge w3-padding din-bold">Team Members</h1>
 	
 		<div class="w3-row" style="margin-left:20px">
-				<h3>Nick Tobia</h3>
-				<p>Nick is a senior consultant for data analytics for social impact at CirroLytix. He specializes in human rights, peace, democracy and justice reform.</p>
+				<h3 class="din">Nick Tobia</h3>
+				<!--<p>Nick is a senior consultant for data analytics for social impact at CirroLytix. He specializes in human rights, peace, democracy and justice reform.</p>-->
 
-				<h3>Miguel Oscar Castelo</h3>
+				<h3 class="din">Miguel Oscar Castelo</h3>
 
-				<h3>Theresa Tan</h3>
+				<h3 class="din">Theresa Tan</h3>
 
-				<h3>Kristel Zapata</h3>
+				<h3 class="din">Kristel Zapata</h3>
 
-				<h3>Helen Mary Labao-Barrameda</h3>
-				<p>Helen is an associate consultant at CirroLytix. She is also a systems and geodetic engineer, web developer, technical writer, and data analyst. he has done projects ranging from GIS, infrastructure development roadmaps, web scraping, database development, systems administration, business intelligence reports, marketing automations, and demand forecasting.</p>
+				<h3 class="din">Helen Mary Labao-Barrameda</h3>
+				<!--<p>Helen is an associate consultant at CirroLytix. She is also a systems and geodetic engineer, web developer, technical writer, and data analyst. he has done projects ranging from GIS, infrastructure development roadmaps, web scraping, database development, systems administration, business intelligence reports, marketing automations, and demand forecasting.</p>-->
 		</div>
 		<footer>
 			<div class="w3-padding w3-black">
