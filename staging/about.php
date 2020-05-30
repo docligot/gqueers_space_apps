a7c6d8c3dc394abaa8b8885f55b26fce59c4bee8
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-05-31 05:38:39 +0800

    Cleaned up About Page

--- staging/about.php
+++ staging/about.php
@@ -10,20 +10,19 @@ function showGideon() {
 		<div class="w3-padding">
 			<p>Many impacts of COVID-19 are reflected in socio-economic and epidemiologic data (e.g. unemployment statistics, infection rates, and mobility and transportation data). However, combining socio-economic data with Earth Observation (EO) data can enhance our understanding of and generate new insights regarding the effects of COVID-19.</p>
 
-			<p>5.9 million people tested positive with Covid-19</p>
-			<p>Only 43% have recovered.</p>
+			<p>At the time of this writing, over 5.9 million people tested positive with Covid-19. Only 43% have recovered.</p>
 
 			<p>While most solutions are looking at direct health impact, the economy is also taking a hit from the pandemic.</p>
 
 			<p>Countries with the most economic damage risk need to take more informed action to save their GDP even before the GDP quarterly reports are out. Low GDP countries do not typically have strong statistical analysis of their economy. Policy makers need to make quicker decisions in the face of the crisis. As such, non-traditional GDP parameters need to be used.</p> 
 
 			<h3 class="w3-large din-bold">THE SOLUTION</h3>
-			<p>Instead of the traditional way, we nowcast GDP from the integration of night light (economic), covid-19 growth rate (health), mobility data (lockdown and human activity), and nitrogen dioxide (environment).</p>
+			<p>Instead of the traditional way, we nowcast GDP from the integration of night light (economic), covid-19 growth rate (health), mobility data (lockdown and human activity), and nitrogen dioxide (environment). The approach is multi-disciplinary and goes beyond the inclusion of typical economic indicators.</p>
 			<p>Satellite data updates more frequently than traditional GDP predictors. Policy makers can take quicker action with insights derived. </p>
 			<h3 class="w3-large din-bold">THIS DASHBOARD PROTOTYPE</h3>
-			<p></p>
+			<p>Our dashboard mainly indicates a health status of a country's upcoming GDP change in the next quarter into three categories: Manageable, Moderate, and Critical. It also allows zooming in on factors of social mobility, economics, and environment damage.</p>
 			<h3 class="w3-large din-bold">FUTURE DEVELOPMENTS</h3>
-			<p><b>Gideon Insights</b> </p>
+			<p><b>Gideon Insights</b> will produce additional historical information and overlay additional variables to improve the data modeling for GDP nowcasting. Due to the limited time of the hackathon, only a few countries' satellite imagery data were reasonably collected to study the indicative relationships. We will also make a more sophisticated geospatial analysis beyond the prototyping stage if given the chance to develop the project.</p>
 		</div>
 		<footer>
 			<div class="w3-padding iact-blue">
