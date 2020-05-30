c721ce0fdbbe066979ea850b89db10b0272dfc1a
tessa-tan <44916413+tessa-tan@users.noreply.github.com>
2020-05-30 12:58:47 +0800

    updated insights page

---
+++ staging/images/loadProfiles.php
@@ -0,0 +1,6 @@
+<?php
+echo '
+  <img class="country_profile w3-hide" id="defaultcountry" src="images/babytaco.jpg" style="width: 100%;" />
+  <img class="country_profile w3-hide" id="philippines" src="images/phils.jpg" style="width: 100%;" />
+  <img class="country_profile w3-hide" id="japan" src="images/japan.jpg" style="width: 100%;" />';
+exit();
