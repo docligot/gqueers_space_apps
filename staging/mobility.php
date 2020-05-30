abec138a129f593ce5b2175dd322fff11e8c3cc4
kristelzapata <44916803+kristelzapata@users.noreply.github.com>
2020-05-30 15:33:44 +0800

    Uploaded mobility page

--- staging/mobility.php
+++ staging/mobility.php
@@ -1,9 +1,90 @@
 <?php
 
 function showMobility() {
+?>
 
-
-
-
+<div class="w3-row">
+	<div class="w3-col l2">&nbsp;</div>
+	<div class="w3-col l10">
+		<h1 class="w3-xlarge w3-padding din-bold">Mobility</h1>
 	
+		<div class="w3-row" style="margin-left:15px">
+		
+			<div class="w3-row">
+				</section>
+					<div class="w3-col l9 w3-padding">
+						<img src="images/mobility/IT.png" style="width:100%; height: 460px;"/>
+					</div>
+				</section>
+				<br/>
+				<br/>
+				<div class="w3-small">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</div>
+			</div>
+			
+			<br/>
+			<br/>
+			<div class="w3-row">
+				</section>
+					<div class="w3-col l9 w3-padding">
+						<img src="images/mobility/PH.png" style="width:100%; height: 460px;"/>
+					</div>
+				</section>
+				<br/>
+				<br/>
+				<div class="w3-small">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</div>
+			</div>		
+			
+			
+			<br/>
+			<br/>
+			<div class="w3-row">
+				</section>
+					<div class="w3-col l9 w3-padding">
+						<img src="images/mobility/SG.png" style="width:100%; height: 460px;"/>
+					</div>
+				</section>
+				<br/>
+				<br/>
+				<div class="w3-small">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</div>
+			</div>		
+
+			
+			<br/>
+			<br/>
+			<div class="w3-row">
+				</section>
+					<div class="w3-col l9 w3-padding">
+						<img src="images/mobility/SD.png" style="width:100%; height: 460px;"/>
+					</div>
+				</section>
+				<br/>
+				<br/>
+				<div class="w3-small">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</div>
+			</div>	
+
+			
+			<br/>
+			<br/>
+			<div class="w3-row">
+				</section>
+					<div class="w3-col l9 w3-padding">
+						<img src="images/mobility/JP.png" style="width:100%; height: 460px;"/>
+					</div>
+				</section>
+				<br/>
+				<br/>
+				<div class="w3-small">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</div>
+			</div>					
+		
+		</div>
+		<footer>
+			<div class="w3-padding w3-black">
+				NASA Space Apps Challenge - Team Gideon by CirroLytix
+			</div>
+		</footer>
+	</div>
+</div>
+
+
+<?php	
 }
--- staging/team.php
+++ staging/team.php
@@ -1,19 +1,31 @@
 <?php
 function showTeam() {
 ?>
-<div id="l2"></div>
-<div id="l10">
-<h1>Nick Tobia</h1>
-<p>Nick is a senior consultant for data analytics for social impact at CirroLytix. He specializes in human rights, peace, democracy and justice reform.</p>
 
-<h1>Miguel Oscar Castelo</h1>
+<div class="w3-row">
+	<div class="w3-col l2">&nbsp;</div>
+	<div class="w3-col l10">
+		<h1 class="w3-xlarge w3-padding din-bold">Team Members</h1>
+	
+		<div class="w3-row" style="margin-left:15px">
+				<h3>Nick Tobia</h3>
+				<p>Nick is a senior consultant for data analytics for social impact at CirroLytix. He specializes in human rights, peace, democracy and justice reform.</p>
 
-<h1>Theresa Tan</h1>
+				<h3>Miguel Oscar Castelo</h3>
 
-<h1>Kristel Zapata</h1>
+				<h3>Theresa Tan</h3>
 
-<h1>Helen Mary Labao-Barrameda</h1>
-<p>Helen is an associate consultant at CirroLytix. She is also a systems and geodetic engineer, web developer, technical writer, and data analyst. he has done projects ranging from GIS, infrastructure development roadmaps, web scraping, database development, systems administration, business intelligence reports, marketing automations, and demand forecasting.</p>
+				<h3>Kristel Zapata</h3>
+
+				<h3>Helen Mary Labao-Barrameda</h3>
+				<p>Helen is an associate consultant at CirroLytix. She is also a systems and geodetic engineer, web developer, technical writer, and data analyst. he has done projects ranging from GIS, infrastructure development roadmaps, web scraping, database development, systems administration, business intelligence reports, marketing automations, and demand forecasting.</p>
+		</div>
+		<footer>
+			<div class="w3-padding w3-black">
+				NASA Space Apps Challenge - Team Gideon by CirroLytix
+			</div>
+		</footer>
+	</div>
 </div>
 
 <?php
