898f71be727ec526288bb1e92efe60b4962983f1
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-05-31 05:30:06 +0800

    Added The Challenge

--- staging/about.php
+++ staging/about.php
@@ -6,9 +6,17 @@ function showGideon() {
 <div class="w3-row">
 	<div class="w3-col l2">&nbsp;</div>
 	<div class="w3-col l10">
-		<h2 class="w3-xlarge w3-padding din-bold">THE CHALLENGE</h2>
+		<h2 class="w3-xlarge w3-padding din-bold">TEAM GIDEON FOR THE NASA SPACE APPS CHALLENGE</h2>
 		<div class="w3-padding">
-			<p></p>
+			<p>Many impacts of COVID-19 are reflected in socio-economic and epidemiologic data (e.g. unemployment statistics, infection rates, and mobility and transportation data). However, combining socio-economic data with Earth Observation (EO) data can enhance our understanding of and generate new insights regarding the effects of COVID-19.</p>
+
+			<p>5.9 million people tested positive with Covid-19</p>
+			<p>Only 43% have recovered.</p>
+
+			<p>While most solutions are looking at direct health impact, the economy is also taking a hit from the pandemic.</p>
+
+			<p>Countries with the most economic damage risk need to take more informed action to save their GDP even before the GDP quarterly reports are out. Low GDP countries do not typically have strong statistical analysis of their economy. Policy makers need to make quicker decisions in the face of the crisis. As such, non-traditional GDP parameters need to be used.</p> 
+
 			<h3 class="w3-large din-bold">THE SOLUTION</h3>
 			<p></p>
 			<h3 class="w3-large din-bold">THIS DASHBOARD PROTOTYPE</h3>
