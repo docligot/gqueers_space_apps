62d02b09655771a481f250fcfd1090ca80add777
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-05-31 14:34:43 +0800

    Fixed team photo filenames and team photos

--- staging/team.php
+++ staging/team.php
@@ -8,20 +8,23 @@ function showTeam() {
 		<h1 class="w3-xlarge w3-padding din-bold">Team Members</h1>
 	
 		<div class="w3-row" style="margin-left:20px">
+				<img src="../teamphotos/nick.png" height="150px" width="200px">
 				<h3 class="din">Nick Tobia</h3>
 				<p>Nick is a senior consultant for data analytics for social impact at CirroLytix. He specializes in human rights, peace, democracy and justice reform.</p>
 
+				<img src="../teamphotos/oscar.png" height="150px" width="200px">
 				<h3 class="din">Miguel Oscar Castelo</h3>
 				<p>Oscar is an associate data analytics consultant at CirroLytix with background in financial markets. He is passionate about data, analytics, investment strategies, and robust problem solving.</p>
 
-
+				<img src="../teamphotos/tessa.jpg" height="150px" width="200px">
 				<h3 class="din">Theresa Rosario Tan</h3>
-				<p>Tessa is an associate data analytics consultant at CirroLytix with a digital marketing and software product management background. A relentless learner, she is passionate about tasks that require critical thinking, analysis, and technical problem solving.</p> 
+				<p>Tessa is an associate data analytics consultant at CirroLytix with a digital marketing and software product management background. A relentless learner, she is passionate about tasks that require critical thinking, analysis, and technical problem solving.</p>
 
-
+				<img src="../teamphotos/kristel.jpg" height="150px" width="200px"> 
 				<h3 class="din">Kristel Joyce Zapata</h3>
 				<p>Kristel is an associate data analytics consultant at CirroLytix. She is also currently involved in policy development for the Philippine Inter-agency Task Force for Covid-19 recommendations. Prior to this, she also has a background in banking and finance.</p>
 
+				<img src="../teamphotos/hm.jpeg" height="150px" width="200px">
 				<h3 class="din">Helen Mary Labao-Barrameda</h3>
 				<p>Helen (also known as H) is an associate data consultant at CirroLytix. She is also a systems and geodetic engineer, web developer, technical writer, and data analyst. She has done projects ranging from GIS, infrastructure development roadmaps, web scraping, database development, systems administration, business intelligence reports, marketing automations, and demand forecasting.</p>
 		</div>
