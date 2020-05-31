0bda0f357ec62cdd6ecb9df0e8f75e721a95059e
kristelzapata <44916803+kristelzapata@users.noreply.github.com>
2020-05-31 12:32:30 +0800

    Update about.php

--- staging/about.php
+++ staging/about.php
@@ -22,6 +22,15 @@ function showGideon() {
 			<p>Instead of the traditional way that waits for quarterly updates to see the integrated generic economic health of a country, we nowcast GDP.</p>
 			<p>Satellite data updates more frequently than traditional GDP predictors. Policy makers can take quicker action with insights derived. Project G.I.D.E.O.N. (Global Impact Detection using Emitted Light, Onset of Covid-19, and Nitrogen Dioxide) makes use of historical quarterly GDP, emitted light (proxy variable for income), onset of covid-19 growth, and environmental factors (nitrogen dioxide content in a country) to predict GDP values sooner.</p>
 			<p>Nowcasting GDP means the countries no longer have to wait until the official GDP quarterly data has been posted before they take action.</p>
+			
+				<p><b>The Data Value Chain</b></p>			
+				</section class="w3-center">
+
+					
+						<img src="images/Data value chain.png" style="width:60%; height: 200px;"/>
+					
+				</section>
+			<br/>
 			<h3 class="w3-large din-bold">THIS BENEFICIARIES</h3>
 
 			<h3 class="w3-large din-bold">THIS DASHBOARD PROTOTYPE</h3>
