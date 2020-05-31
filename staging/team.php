cb7d44cd49a8b07ba346723d4dc0c5e6ef0bb4c2
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-05-31 14:15:53 +0800

    Added team writeups

--- staging/team.php
+++ staging/team.php
@@ -12,13 +12,18 @@ function showTeam() {
 				<!--<p>Nick is a senior consultant for data analytics for social impact at CirroLytix. He specializes in human rights, peace, democracy and justice reform.</p>-->
 
 				<h3 class="din">Miguel Oscar Castelo</h3>
+				<p>Oscar is a data analytics consultant with background in financial markets. He is passionate about data, analytics, investment strategies, and robust problem solving.</p>
 
-				<h3 class="din">Theresa Tan</h3>
 
-				<h3 class="din">Kristel Zapata</h3>
+				<h3 class="din">Theresa Rosario Tan</h3>
+				<p>Tessa is a data analytics consultant with a digital marketing and software product management background. A relentless learner, she is passionate about tasks that require critical thinking, analysis, and technical problem solving.</p> 
+
+
+				<h3 class="din">Kristel Joyce Zapata</h3>
+				<p>Kristel is a data analytics consultant. She is also currently involved in policy development for the Philippine Inter-agency Task Force for Covid-19 recommendations. Prior to this, she also has a background in banking and finance.</p>
 
 				<h3 class="din">Helen Mary Labao-Barrameda</h3>
-				<!--<p>Helen is an associate consultant at CirroLytix. She is also a systems and geodetic engineer, web developer, technical writer, and data analyst. he has done projects ranging from GIS, infrastructure development roadmaps, web scraping, database development, systems administration, business intelligence reports, marketing automations, and demand forecasting.</p>-->
+				<p>Helen (also known as H) is an associate data consultant. She is also a systems and geodetic engineer, web developer, technical writer, and data analyst. She has done projects ranging from GIS, infrastructure development roadmaps, web scraping, database development, systems administration, business intelligence reports, marketing automations, and demand forecasting.</p>
 		</div>
 		<footer>
 			<div class="w3-padding w3-black">
