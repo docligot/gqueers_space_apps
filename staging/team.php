eda7cada18e13b6cd3a935f3f52067cfec1d7f7b
tessa-tan <44916413+tessa-tan@users.noreply.github.com>
2020-05-31 21:59:54 +0800

    update team page

--- staging/team.php
+++ staging/team.php
@@ -4,33 +4,68 @@ function showTeam() {
 
 <div class="w3-row">
 	<div class="w3-col l2">&nbsp;</div>
-	<div class="w3-col l10">
+	<div class="w3-col l10 w3-display-container">
+
 		<h1 class="w3-xlarge w3-padding din-bold">Team Members</h1>
 
-		<div class="w3-row" style="margin-left:20px">
-			<div class="profile-pic"></div>
-				<img src="../teamphotos/nick.png" height="150px" width="200px">
-				<h3 class="din">Nick Tobia</h3>
-				<p>Nick is a senior consultant for data analytics for social impact at CirroLytix. He specializes in human rights, peace, democracy and justice reform.</p>
+		<div class="w3-row" style="margin-left:5em;">
+			<div class="w3-container w3-cell w3-col l2 w3-card w3-center w3-dark-grey">
+				<div class="w3-cell w3-center">
+					<h4 class="din-bold">Nick Tobia</h4>
+					<img src="teamphotos/nick.png">
+					<p>Nick is a senior consultant for data analytics for social impact at CirroLytix. He specializes in human rights, peace, democracy and justice reform.
+				</div>
+			</div>
+			<div class="w3-container w3-cell w3-col" style="width:2%;">&nbsp;</div>
 
-				<img src="../teamphotos/oscar.png" height="150px" width="200px">
-				<h3 class="din">Miguel Oscar Castelo</h3>
-				<p>Oscar is an associate data analytics consultant at CirroLytix with background in financial markets. He is passionate about data, analytics, investment strategies, and robust problem solving.</p>
+			<div class="w3-container w3-cell w3-col l2 w3-card w3-center w3-dark-grey">
+				<div class="w3-cell w3-center">
+					<h4 class="din-bold">Miguel Oscar Castelo</h4>
+					<img src="teamphotos/oscar.png">
+					<p>Oscar is an associate data analytics consultant at CirroLytix with background in financial markets. He is passionate about data, analytics, investment strategies, and robust problem solving.
+				</div>
+			</div>
 
-				<img src="../teamphotos/tessa.jpg" height="150px" width="200px">
-				<h3 class="din">Theresa Rosario Tan</h3>
-				<p>Tessa is an associate data analytics consultant at CirroLytix with a digital marketing and software product management background. A relentless learner, she is passionate about tasks that require critical thinking, analysis, and technical problem solving.</p>
+			<div class="w3-container w3-cell w3-col" style="width:2%;">&nbsp;</div>
 
-				<img src="../teamphotos/kristel.jpg" height="150px" width="200px">
-				<h3 class="din">Kristel Joyce Zapata</h3>
-				<p>Kristel is an associate data analytics consultant at CirroLytix. She is also currently involved in policy development for the Philippine Inter-agency Task Force for Covid-19 recommendations. Prior to this, she also has a background in banking and finance.</p>
+			<div class="w3-cell w3-col l2 w3-card w3-center w3-dark-grey">
+				<div class="w3-container w3-center">
+					<h4 class="din-bold">Theresa Rosario Tan</h4>
+					<img src="teamphotos/tessa.jpg">
+					<p>Tessa is an analytics consultant at CirroLytix with a digital marketing and software product management background. A relentless learner, she is passionate about tasks that require critical thinking, analysis, and technical problem solving.
+				</div>
+			</div>
 
-				<img src="../teamphotos/hm.jpeg" height="150px" width="200px">
-				<h3 class="din">Helen Mary Labao-Barrameda</h3>
-				<p>Helen (also known as H) is an associate data consultant at CirroLytix. She is also a systems and geodetic engineer, web developer, technical writer, and data analyst. She has done projects ranging from GIS, infrastructure development roadmaps, web scraping, database development, systems administration, business intelligence reports, marketing automations, and demand forecasting.</p>
+			<div class="w3-cell w3-col" style="width:2%;">&nbsp;</div>
+
+			<div class="w3-cell w3-col l2 w3-card w3-center w3-dark-grey">
+				<div class="w3-container w3-center">
+					<h4 class="din-bold">Kristel Joyce Zapata</h4>
+					<img src="teamphotos/kristel.jpg">
+					<p>Kristel is an associate data analytics consultant at CirroLytix. She is also currently involved in policy development for the Philippine Inter-agency Task Force for Covid-19 recommendations. Prior to this, she also has a background in banking and finance.
+				</div>
+			</div>
+
+			<div class="w3-cell w3-col" style="width:2%;">&nbsp;</div>
+
+			<div class="w3-cell w3-col l2 w3-card w3-center w3-dark-grey">
+				<div class="w3-container w3-center">
+					<h4 class="din-bold">Helen Labao-Barrameda</h4>
+					<img src="teamphotos/hm.jpeg">
+					<p>Helen is an analytics consultant at CirroLytix. She is also a systems and geodetic engineer, web developer, technical writer, and data analyst. She has done projects ranging from GIS, database development, systems administration, business intelligence reports, and marketing automations
+				</div>
+			</div>
+
+
 		</div>
+	</div>
+</div>
+
+<div class="w3-row">
+	<div class="w3-col l2">&nbsp;</div>
+	<div class="w3-col l10">
 		<footer>
-			<div class="w3-padding w3-black">
+			<div class="w3-padding w3-black" style="position:fixed; bottom:0; width:100%">
 				NASA Space Apps Challenge - Powered by Team Gideon
 			</div>
 		</footer>
