a3b52fe46ca22435a5bf69a0db6b50c9011457cd
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-05-31 14:17:16 +0800

    Finalized writeups

--- staging/team.php
+++ staging/team.php
@@ -9,21 +9,21 @@ function showTeam() {
 	
 		<div class="w3-row" style="margin-left:20px">
 				<h3 class="din">Nick Tobia</h3>
-				<!--<p>Nick is a senior consultant for data analytics for social impact at CirroLytix. He specializes in human rights, peace, democracy and justice reform.</p>-->
+				<p>Nick is a senior consultant for data analytics for social impact at CirroLytix. He specializes in human rights, peace, democracy and justice reform.</p>
 
 				<h3 class="din">Miguel Oscar Castelo</h3>
-				<p>Oscar is a data analytics consultant with background in financial markets. He is passionate about data, analytics, investment strategies, and robust problem solving.</p>
+				<p>Oscar is an associate data analytics consultant at CirroLytix with background in financial markets. He is passionate about data, analytics, investment strategies, and robust problem solving.</p>
 
 
 				<h3 class="din">Theresa Rosario Tan</h3>
-				<p>Tessa is a data analytics consultant with a digital marketing and software product management background. A relentless learner, she is passionate about tasks that require critical thinking, analysis, and technical problem solving.</p> 
+				<p>Tessa is an associate data analytics consultant at CirroLytix with a digital marketing and software product management background. A relentless learner, she is passionate about tasks that require critical thinking, analysis, and technical problem solving.</p> 
 
 
 				<h3 class="din">Kristel Joyce Zapata</h3>
-				<p>Kristel is a data analytics consultant. She is also currently involved in policy development for the Philippine Inter-agency Task Force for Covid-19 recommendations. Prior to this, she also has a background in banking and finance.</p>
+				<p>Kristel is an associate data analytics consultant at CirroLytix. She is also currently involved in policy development for the Philippine Inter-agency Task Force for Covid-19 recommendations. Prior to this, she also has a background in banking and finance.</p>
 
 				<h3 class="din">Helen Mary Labao-Barrameda</h3>
-				<p>Helen (also known as H) is an associate data consultant. She is also a systems and geodetic engineer, web developer, technical writer, and data analyst. She has done projects ranging from GIS, infrastructure development roadmaps, web scraping, database development, systems administration, business intelligence reports, marketing automations, and demand forecasting.</p>
+				<p>Helen (also known as H) is an associate data consultant at CirroLytix. She is also a systems and geodetic engineer, web developer, technical writer, and data analyst. She has done projects ranging from GIS, infrastructure development roadmaps, web scraping, database development, systems administration, business intelligence reports, marketing automations, and demand forecasting.</p>
 		</div>
 		<footer>
 			<div class="w3-padding w3-black">
