13e2e12505bbc8e5a865ee878efa4055698114d4
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-05-31 10:28:01 +0800

    Added JHU data source

--- staging/about.php
+++ staging/about.php
@@ -8,7 +8,7 @@ function showGideon() {
 	<div class="w3-col l10">
 		<h2 class="w3-xlarge w3-padding din-bold">TEAM GIDEON FOR THE NASA SPACE APPS CHALLENGE</h2>
 		<div class="w3-padding">
-			<p>General Impact Detection using Emitted Light, Onset of Covid-19, and Nitrogen Dioxide<br>The odds of the pandemic to the economy are against us.<br>Let's beat the odds with satellite data using GIDEON's nowcast tool. </p>
+			<p>Global Impact Detection using Emitted Light, Onset of Covid-19, and Nitrogen Dioxide<br>The odds of the pandemic to the economy are against us.<br>Let's beat the odds with satellite data using GIDEON's nowcast tool. </p>
 
 			<p>Many impacts of COVID-19 are reflected in socio-economic and epidemiologic data (e.g. unemployment statistics, infection rates, and mobility and transportation data). However, combining socio-economic data with Earth Observation (EO) data can enhance our understanding of and generate new insights regarding the effects of COVID-19.</p>
 
@@ -20,7 +20,7 @@ function showGideon() {
 
 			<h3 class="w3-large din-bold">THE SOLUTION</h3>
 			<p>Instead of the traditional way that waits for quarterly updates to see the integrated generic economic health of a country, we nowcast GDP.</p>
-			<p>Satellite data updates more frequently than traditional GDP predictors. Policy makers can take quicker action with insights derived. Project G.I.D.E.O.N. (General Impact Detection using Emitted Light, Onset of Covid-19, and Nitrogen Dioxide) makes use of historical quarterly GDP, emitted light (proxy variable for income), onset of covid-19 growth, and environmental factors (nitrogen dioxide content in a country) to predict GDP values sooner.</p>
+			<p>Satellite data updates more frequently than traditional GDP predictors. Policy makers can take quicker action with insights derived. Project G.I.D.E.O.N. (Global Impact Detection using Emitted Light, Onset of Covid-19, and Nitrogen Dioxide) makes use of historical quarterly GDP, emitted light (proxy variable for income), onset of covid-19 growth, and environmental factors (nitrogen dioxide content in a country) to predict GDP values sooner.</p>
 			<p>Nowcasting GDP means the countries no longer have to wait until the official GDP quarterly data has been posted before they take action.</p>
 			<h3 class="w3-large din-bold">THIS BENEFICIARIES</h3>
 
@@ -38,6 +38,7 @@ function showGideon() {
 			<ol>- [Map] AirPollutant Concentration Data from ArcGIS Online</ol>
 			<ol>- Google's Mobility Data during Global Lockdowns https://www.google.com/covid19/mobility/</ol>
 			<ol> Covid-19 Growth Rate and Case Data from John Hopkins University and CoronaTracker.com</ol>
+			<h3 class="w3-large din-bold">RELATED LITERATURE</h3>
 		</div>
 		<footer>
 			<div class="w3-padding w3-black">
