66b9ac3c3f7d8f22aabbb62628cc4133ddc52f11
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2020-05-31 08:37:36 +0800

    Modified about a little

--- staging/about.php
+++ staging/about.php
@@ -8,6 +8,8 @@ function showGideon() {
 	<div class="w3-col l10">
 		<h2 class="w3-xlarge w3-padding din-bold">TEAM GIDEON FOR THE NASA SPACE APPS CHALLENGE</h2>
 		<div class="w3-padding">
+			<p>General Impact Detection using Emitted Light, Onset of Covid-19, and Nitrogen Dioxide<br>The odds of the pandemic to the economy are against us.<br>Let's beat the odds with satellite data using GIDEON's nowcast tool. </p>
+
 			<p>Many impacts of COVID-19 are reflected in socio-economic and epidemiologic data (e.g. unemployment statistics, infection rates, and mobility and transportation data). However, combining socio-economic data with Earth Observation (EO) data can enhance our understanding of and generate new insights regarding the effects of COVID-19.</p>
 
 			<p>At the time of this writing, over 5.9 million people tested positive with Covid-19. Only 43% have recovered.</p>
@@ -17,8 +19,10 @@ function showGideon() {
 			<p>Countries with the most economic damage risk need to take more informed action to save their GDP even before the GDP quarterly reports are out. Low GDP countries do not typically have strong statistical analysis of their economy. Policy makers need to make quicker decisions in the face of the crisis. As such, non-traditional GDP parameters need to be used.</p> 
 
 			<h3 class="w3-large din-bold">THE SOLUTION</h3>
-			<p>Instead of the traditional way, we nowcast GDP from the integration of night light (economic), covid-19 growth rate (health), mobility data (lockdown and human activity), and nitrogen dioxide (environment). The approach is multi-disciplinary and goes beyond the inclusion of typical economic indicators.</p>
-			<p>Satellite data updates more frequently than traditional GDP predictors. Policy makers can take quicker action with insights derived. </p>
+			<p>Instead of the traditional way that waits for quarterly updates to see the integrated generic economic health of a country, we nowcast GDP.</p>
+			<p>Satellite data updates more frequently than traditional GDP predictors. Policy makers can take quicker action with insights derived. Project G.I.D.E.O.N. (General Impact Detection using Emitted Light, Onset of Covid-19, and Nitrogen Dioxide) makes use of historical quarterly GDP, emitted light (proxy variable for income), onset of covid-19 growth, and environmental factors (nitrogen dioxide content in a country) to predict GDP values sooner.</p>
+			<p>It scores countries' GDP forecasted change as light, moderate or heavy/critical to help policy makers craft their decisions with basis from data. The "traffic light" system produced by our solution can inform policy makers about certain economic decisions. If the dip in the GDP is caused by mobility, for example, easing lockdown may be essential to jumpstart the economic activity of the country and have the GDP recovery.</p>
+			<p>Nowcasting GDP means the countries no longer have to wait until the official GDP quarterly data has been posted before they take action.</p>
 			<h3 class="w3-large din-bold">THIS DASHBOARD PROTOTYPE</h3>
 			<p>Our dashboard mainly indicates a health status of a country's upcoming GDP change in the next quarter into three categories: Manageable, Moderate, and Critical. It also allows zooming in on factors of social mobility, economics, and environment damage.</p>
 			<h3 class="w3-large din-bold">FUTURE DEVELOPMENTS</h3>
