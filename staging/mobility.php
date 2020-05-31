c937cbed18de0efaa5e818ba47b009b2a641a308
StellarBlackCat <45113885+StellarBlackCat@users.noreply.github.com>
2020-05-31 16:58:11 +0800

    push

--- staging/mobility.php
+++ staging/mobility.php
@@ -2,92 +2,131 @@
 
 function showMobility() {
 ?>
+<script src="mobility/renderMobility.js"></script>
+<style>
+.lds-heart {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+  transform: rotate(45deg);
+  transform-origin: 40px 40px;
+}
+.lds-heart div {
+  top: 32px;
+  left: 32px;
+  position: absolute;
+  width: 32px;
+  height: 32px;
+  background: #fff;
+  animation: lds-heart 1.2s infinite cubic-bezier(0.215, 0.61, 0.355, 1);
+}
+.lds-heart div:after,
+.lds-heart div:before {
+  content: " ";
+  position: absolute;
+  display: block;
+  width: 32px;
+  height: 32px;
+  background: #fff;
+}
+.lds-heart div:before {
+  left: -24px;
+  border-radius: 50% 0 0 50%;
+}
+.lds-heart div:after {
+  top: -24px;
+  border-radius: 50% 50% 0 0;
+}
+@keyframes lds-heart {
+  0% {
+    transform: scale(0.95);
+  }
+  5% {
+    transform: scale(1.1);
+  }
+  39% {
+    transform: scale(0.85);
+  }
+  45% {
+    transform: scale(1);
+  }
+  60% {
+    transform: scale(0.95);
+  }
+  100% {
+    transform: scale(0.9);
+  }
+}
+</style>
 
-<div class="w3-row">
+
+<div>
 	<div class="w3-col l2">&nbsp;</div>
 	<div class="w3-col l10">
-		<h1 class="w3-xlarge w3-padding din-bold">Mobility</h1>
-	
-		<div class="w3-row" style="margin-left:15px">
-		
-			<div class="w3-row w3-padding">
-				</section>
-					<div class="w3-col l9 w3-padding">
-					<h3>Philippines</h3>
-						<img src="images/mobility/PH.png" style="width:100%; height: 460px;"/>
-					</div>
-				</section>
-				<br/>
-				<br/>
-				<br/>
-				<div class="w3-small w3-padding">Lockdown measures have people staying in their homes in the Philippines as shown by the yellow line. Lockdowns are still in place as of May for the Filipino people. There is no visible resurgence or uptick in covid growths after lockdown measures are slowly being lifted.</div>
-			</div>		
-			<br/>
-			<br/>
-			<div class="w3-row w3-padding">
-				</section>
-					<div class="w3-col l9 w3-padding">
-					<h3>Japan</h3>
-						<img src="images/mobility/SD.png" style="width:100%; height: 460px;"/>
-					</div>
-				</section>
-				<br/>
-				<br/>
-				<br/>
-				<div class="w3-small w3-padding">Japan did not issue any strict lockdowns despite the covid-19 cases they had to face locally and in the Diamond Princess ship. However, a notable decrease in mobility in transportation has been observed in the height of the covid-19 pandemic.</div>
+		<h2 class="w3-xlarge w3-padding din-bold">Country Profiles</h2>
+
+	<div>
+			<div class="w3-col l3 w3-padding">
+				<select id="variable1" class="w3-select" onchange="extractMobility();">
+					<option value="philippines">Philippines</option>
+					<option value="japan">Japan</option>
+					<option value="Singapore">Singapore</option>
+					<option value="Italy">Italy</option>
+					<option value="Sweden">Sweden</option>
+				</select>
+		<hr/>
+		<div>
+				<div id="country_container" style="width:100%; height:460px;"></div>
+				<!--<div id="no2_container"></div>-->
+				<canvas id="canvas" style="width:100%; height: 190px;"></canvas>
+
 			</div>
-			<br/>
-			<br/>
-			<div class="w3-row w3-padding">
-				</section>
-					<div class="w3-col l9 w3-padding">
-					<h3>Italy</h3>
-						<img src="images/mobility/IT.png" style="width:100%; height: 460px;"/>
-					</div>
-				</section>
-				<br/>
-				<br/>
-				<br/>
-				<div class="w3-small w3-padding">The spike in yellow line indicates the onset of lockdown in Italy and the bars show the actual number of covid growth cases in the country at the time the mobility data was captured. The slightly increasing activity at the parks indicate that lives are slowly going back to normal for Italians by May 2020 compared to its low during March and April.</div>
-			</div>
-			<br/>
-			<br/>
-			<div class="w3-row w3-padding">
-				</section>
-					<div class="w3-col l9 w3-padding">
-					<h3>Singapore</h3>
-						<img src="images/mobility/SG.png" style="width:100%; height: 460px;"/>
-					</div>
-				</section>
-				<br/>
-				<br/>
-				<br/>
-				<div class="w3-small w3-padding">Singapore's social distancing and protocols are visibly moderate compared to other countries with dramatic drop in mobility.</div>
-			</div>
-			<br/>
-			<br/>
-			<div class="w3-row w3-padding">
-				</section>
-					<div class="w3-col l9 w3-padding">
-					<h3>Sweden</h3>
-						<img src="images/mobility/JP.png" style="width:100%; height: 460px;"/>
-					</div>
-				</section>
-				<br/>
-				<br/>
-				<br/>
-				<div class="w3-small w3-padding">Sweden's controversial zero lockdown strategy ruffled feathers globally. Despite that, it looks like covid-19 infection numbers is still relatively somehow well-managed.</div>
-			</div>					
-		
 		</div>
+		
+		
 		<footer>
 			<div class="w3-padding w3-black">
 				NASA Space Apps Challenge - Powered by Team Gideon
 			</div>
 		</footer>
-	</div>
+
+</div>
 </div>
 
+<script>
+function loadDefaultCountryPro() {
+	var xmlhttp = new XMLHttpRequest();
 
-<?php	
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById('country_container').innerHTML = this.responseText;
+			document.getElementById('defaultcountry').className += " w3-hide";
+		} else {
+			document.getElementById('country_container').innerHTML = "<div class=\"w3-display-container\" style=\"height: 400px;\"><div class=\"lds-heart w3-display-middle\"><div></div></div></div>";
+		}
+	}
+	xmlhttp.open("GET", "images/loadMobility.php", true);
+	xmlhttp.send();
+}
+loadDefaultCountryPro();
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 500); // Change image every 2 seconds
+}
+</script>
+
+
+<?php
 }
